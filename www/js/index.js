e9aa5e1bb36ee1d1041c81101e0d71f8303d1953
goparklubanic <ngoparios@yahoo.com>
2018-01-19 15:19:26 +0700

    eval 3 login script

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,34 @@
 $(document).ready(function(){
-  $('#login').click(function(){
+	$("#LoginLog").html('Bersiap Login');
+	$( "#login-form" ).submit(function( event ) {
+ 
+		// Stop form from submitting normally
+		event.preventDefault();
+ 
+		// Send the data using post
+		var posting = $.post( serviceUrl+'warga.php', { 
+			does  : 'kulanuwun',
+			uname : $('#username').val(),
+			upass : $('#password').val()
+		} );
+ 
+		// Put the results in a div
+		posting.done(function( data ) {
+			
+			var user = JSON.parse(data);
+			$('#LoginLog').append(
+			"<p>NIK     :"+user.nik+"</p>"+
+			"<p>Nama    :"+user.nama+"</p>"+
+			"<p>L/P     :"+user.kelamin+"</p>"+
+			"<p>Tg Lahir:"+user.tgLahir+"</p>"+
+			"<p>RT      :"+user.rt+"</p>"+
+			"<p>RW      :"+user.rw+"</p>"
+			);
+		});
+		
+		posting.fail(function(){
+			$("#LoginLog").html('Error Tak Dikenal');
+		});
+	});
 	
-    $.post(
-      serviceUrl+'warga.php',{
-        does  : 'kulanuwun',
-        uname : $('#username').val(),
-        upass : $('#password').val()
-      },
-      function(respon){
-        console.log(respon);
-        var user = JSON.parse(respon);
-        $('#LoginLog').html(respon);
-      });
-  });
 });
