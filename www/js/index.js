01a5545568502552a2f374b07f6a23c64f4bdaca
goparklubanic <ngoparios@yahoo.com>
2018-01-19 14:38:01 +0700

    eval 2 login script

--- www/js/index.js
+++ www/js/index.js
@@ -8,22 +8,9 @@ $(document).ready(function(){
         upass : $('#password').val()
       },
       function(respon){
-        // console.log(respon);
+        console.log(respon);
         var user = JSON.parse(respon);
-        if(user.nik == null ){
-			window.location='galo.html';
-		}else{
-		$('#LoginLog').html(respon);
-		/*
-        $("#LoginLog").html('Login Berhasil');
-        localStorage.setItem('nik',user.nik);
-        localStorage.setItem('nama',user.nama);
-        localStorage.setItem('sex',user.kelamin);
-        localStorage.setItem('lahir',user.tgLahir);
-        localStorage.setItem('rtrw',user.rt + user.rw);
-        window.location='index.html';
-		*/
-		}
+        $('#LoginLog').html(respon);
       });
   });
 });
